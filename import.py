966a94ba4c3f249b68fa6105a33022bf162bb387
SLYR <peteraltforstuff@gmail.com>
2025-08-29 13:02:07 -0400

    improvements

--- import.py
+++ import.py
@@ -1,12 +1,23 @@
-
 Cash = int(1000)
+While True:
+try:
+    Bet = int(input("Welcome to Roulette! How much would you like to bet today? (You have: " + str(Cash) + " Dollars!)"))
+    if Bet <= 0
+        Print("Bet must be more than $0!")
+    elif Bet > Cash
+        Print("Not enough money!")
+    elif Cash = 0
 
-Bet = int(input("Welcome to Roulette! How much would you like to bet today? (You have: " (Cash)  "Dollars!"))
 
-Bet_int = int(Bet)
+current_bet = (Bet-Cash)
 
-current_bet = (Bet_int-Cash)
 
-if Cash<0:
+if Cash<Bet:
     print("YOU'RE BROKE!! (Out of cash! Sorry!) GET OUT!!!")
-    else print("Okay!")
+else: print("Okay!")
+
+Bet_Number = input("Choose what to bet on: 1. Specific Number, 2. Specific Color")
+
+if Bet_Number = 1
+    choice = int(input("What number do you want to bet on? (00, 0-36)"))
+if Bet_Number
