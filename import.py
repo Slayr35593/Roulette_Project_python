da22cbcffa5d7920786556e984b10635f8129369
SLYR <peteraltforstuff@gmail.com>
2025-08-28 21:47:27 -0400

    just started code still sucks.

---
+++ import.py
@@ -0,0 +1,12 @@
+
+Cash = int(1000)
+
+Bet = int(input("Welcome to Roulette! How much would you like to bet today? (You have: {Cash} Dollars!"))
+
+Bet_int = int(Bet)
+
+current_bet = (Bet_int-Cash)
+
+if Cash<0:
+    Print("YOU'RE BROKE!! (Out of cash! Sorry!) GET OUT!!!")
+    
